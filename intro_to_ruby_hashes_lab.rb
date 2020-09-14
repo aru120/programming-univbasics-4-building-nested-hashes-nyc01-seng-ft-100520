97c13391b6dc29e4f9c709f7b6bc53090044289a
Kevin Huang <khuang159@gmail.com>
2020-09-14 17:22:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,8 @@ def monopoly_with_second_tier
   # complexity.
   
   baseH = Hash.new
-   baseH[:railroads] = {:pieces => 4, :rent_in_dollars => {},:names =>{}}
+  baseH = {:railroads => {}}
+  baseH[:railroads] = {:pieces => 4, :rent_in_dollars => {},:names =>{}}
 
 end
 
